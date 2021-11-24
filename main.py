cc268225766ba7510793938c1b1848ef62fb187f
khashayar7kh78 <84039941+khashayar7kh78@users.noreply.github.com>
2021-11-25 00:39:59 +0330

    done

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from tkinter import *
 
 
+def read_info():
+    file = open("user.txt","r")
+    i = 0
+    var = [""]
+    while True :
+        st = file.readline()
+        var.insert(i,st)
+        i+=1
+        if st == "":
+            break
+    file.close()
+    return var
+
 def save_info():
     firstname_info = firstname.get()
     lastname_info = lastname.get()
@@ -11,11 +24,11 @@ def save_info():
 
     file = open("user.txt", "a")
     file.write(firstname_info)
-    file.write("*")
+    file.write("    ")
     file.write(lastname_info)
-    file.write("*")
+    file.write("    ")
     file.write(studentid_info)
-    file.write("*")
+    file.write("    ")
     file.write(age_info)
     file.write("\n")
     file.close()
@@ -23,20 +36,29 @@ def save_info():
 
     firstname_entry.delete(0, END)
     lastname_entry.delete(0, END)
-    studentid_entry.delete(0,END)
+    studentid_entry.delete(0, END)
     age_entry.delete(0, END)
 
 
 screen = Tk()
-screen.geometry("500x500")
+screen.geometry("800x600",)
 screen.title("Moshakhasate Daneshjo")
-heading = Label(text="Python Form", bg="grey", fg="black", width="500", height="3")
+heading = Label(text="Moshakhasate Daneshjo", bg="skyBlue", fg="black", width="500", height="3")
 heading.pack()
 
-firstname_text = Label(text="Firstname * ", )
-lastname_text = Label(text="Lastname * ", )
-studentid_text = Label(text="Student ID *", )
-age_text = Label(text="Age * ", )
+listView = Listbox(width=50,)
+p = read_info()
+print(p)
+o1 = 1
+for o in range (0,len(p)):
+    listView.insert(o1,p.__getitem__(o))
+    o1+=1
+listView.place(x=300, y=90)
+
+firstname_text = Label(text="Firstname : ",)
+lastname_text = Label(text="Lastname : ", )
+studentid_text = Label(text="Student ID : ", )
+age_text = Label(text="Age : ", )
 firstname_text.place(x=15, y=70)
 lastname_text.place(x=15, y=120)
 studentid_text.place(x=15, y=170)
@@ -56,6 +78,8 @@ lastname_entry.place(x=15, y=140)
 studentid_entry.place(x=15, y=190)
 age_entry.place(x=15, y=240)
 
-register = Button(screen, text="Register", width="25", height="2", command=save_info, bg="grey")
+register = Button(screen, text="Register", width="25", height="2", command=save_info, bg="skyBlue")
 register.place(x=15, y=290)
+remove = Button(screen, text="Remove", width="25",height="2", bg="skyBlue")
+remove.place(x=15, y=500)
 screen.mainloop()
